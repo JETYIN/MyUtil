8972ea6dcf45805c13267caa1c67ee7c63f94988
Dylan <494699974@qq.com>
2018-01-25 18:17:28 +0800

    获取本地手机图片uri

--- app/src/main/java/utils/DensityUtil.java
+++ app/src/main/java/utils/DensityUtil.java
@@ -34,4 +34,30 @@ public class DensityUtil {
         BitmapFactory.decodeFile(pathName, opts);
         return new int[]{opts.outWidth, opts.outHeight};
     }
+
+
+    /***
+     * 拥有虚拟键的dialog设置，全屏时隐藏虚拟键，全屏显示
+     */
+    private void setHideDialog() {
+        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        this.getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        // 布局位于状态栏下方
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        // 全屏
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        // 隐藏导航栏
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+                if (Build.VERSION.SDK_INT >= 19) {
+                    uiOptions |= 0x00001000;
+                } else {
+                    uiOptions |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+                }
+                AbstractDialog.this.getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+            }
+        });
+    }
 }
